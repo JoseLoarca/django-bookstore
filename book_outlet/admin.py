9683bc9b41a8e8345a167259ff5dc95a050ed924
José Loarca <joseloarca97@icloud.com>
2022-01-31 23:41:50 -0600

    implement relationships

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 
 
 # Register your models here
@@ -7,8 +7,27 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('title', 'rating', 'author')
-    list_display = ('title', 'author')
-    search_fields = ('title', 'author')
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('first_name', 'last_name',)
+    search_fields = ('first_name',)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ('street', 'postal_code', 'city')
+    search_fields = ('street', 'city')
+    list_display = ('street', 'postal_code', 'city')
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'code')
+    search_fields = ('name', 'code')
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country, CountryAdmin)
